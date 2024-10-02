6663c9ea9eb70552cb72856b86948f447cb1f84a
Dylan Adams <113365542+max5max46@users.noreply.github.com>
2024-10-02 10:31:33 -0300

    Some Small Changes

--- Assets/Scripts/Sickling.cs
+++ Assets/Scripts/Sickling.cs
@@ -9,9 +9,15 @@ public class Sickling : MonoBehaviour
     [SerializeField] private float visionDistance = 10;
     [SerializeField] private Color healthyColor;
     [SerializeField] private Color sickColor;
+    [SerializeField] private float maxStepTime = 2;
 
     [HideInInspector] public bool isSick;
-    [HideInInspector] public float[] distanceFromOtherSicklings;
+    [HideInInspector] public GameObject[] otherSicklings;
+
+    private float stepTimer = 0;
+    private Vector3 randomMoveDirection;
+    private Vector3 moveDirection;
+    private Vector3 nearistSickling;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +37,23 @@ public class Sickling : MonoBehaviour
             transform.GetComponent<SpriteRenderer>().color = sickColor;
         }
 
-        transform.position += transform.up * Time.deltaTime * speed;
+        for (int i = 0; i < otherSicklings.Length; i++)
+        {
+            if (Vector3.Distance(transform.position, otherSicklings[i].transform.position) < Vector3.Distance(transform.position, nearistSickling))
+            {
+
+            }
+        }
+
+        stepTimer -= Time.deltaTime;
+
+        if (stepTimer <= 0)
+        {
+            randomMoveDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+            stepTimer = Random.Range(0.5f, maxStepTime);
+        }
+
+        // Move Random
+        transform.position += moveDirection * Time.deltaTime * speed;
     }
 }
